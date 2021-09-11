ec44ff838966bdc97dc1e682841fe7c9ccdfa704
satiwan kumar <satiwankumar12@gmail.com>
2021-09-11 12:55:07 +0500

    updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const engines = require("consolidate");
 var cors = require("cors");
